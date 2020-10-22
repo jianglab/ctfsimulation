db07b14bdc1040102c70f27c4edd48541f507007
Wen Jiang <wjiang@gmail.com>
2020-10-22 00:49:38 -0400

    fix setup.sh

--- setup.sh
+++ setup.sh
@@ -1,11 +1,11 @@
 mkdir -p ~/.streamlit/
-echo “\
-[general]\n\
-email = \”your-email@domain.com\”\n\
-“ > ~/.streamlit/credentials.toml
-echo “\
-[server]\n\
-headless = true\n\
-enableCORS=false\n\
-port = $PORT\n\
-“ > ~/.streamlit/config.toml
+echo '
+[general]
+email = ”your-email@domain.com”
+' > ~/.streamlit/credentials.toml
+echo '
+[server]
+headless = true
+enableCORS=false
+port = $PORT
+' > ~/.streamlit/config.toml
