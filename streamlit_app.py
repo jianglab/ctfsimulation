b559717116560114367e98a1aefb0ae495e52fc4
Wen Jiang <wjiang@gmail.com>
2021-01-21 14:49:52 -0500

    plot 4 curves when astigmatism mag is nonzero

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,43 @@
+""" 
+MIT License
+
+Copyright (c) 2020-2021 Wen Jiang
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+"""
+
+def import_with_auto_install(packages, scope=locals()):
+    if isinstance(packages, str): packages=[packages]
+    for package in packages:
+        if package.find(":")!=-1:
+            package_import_name, package_pip_name = package.split(":")
+        else:
+            package_import_name, package_pip_name = package, package
+        try:
+            scope[package_import_name] = __import__(package_import_name)
+        except ImportError:
+            import subprocess
+            subprocess.call(f'pip install {package_pip_name}', shell=True)
+            scope[package_import_name] =  __import__(package_import_name)
+required_packages = "streamlit numpy scipy bokeh skimage:scikit_image".split()
+import_with_auto_install(required_packages)
+
 import streamlit as st
 import numpy as np
 import base64
@@ -5,7 +45,9 @@ import base64
 def main():
     session_state = SessionState_get(defocus=0.5, emd_id=0)
 
-    st.set_page_config(page_title="CTF Simulation", layout="wide")
+    title = "CTF Simulation"
+    st.set_page_config(page_title=title, layout="wide")
+    st.title(title)
 
     with st.sidebar:    # sidebar at the left of the screen
         options = ('CTF', '|CTF|', 'CTF^2')
@@ -37,41 +79,92 @@ def main():
         ampcontrast = st.number_input('ampltude contrast (percent)', value=10., min_value=0.0, max_value=100., step=10.0)
 
     # right-side main panel
-    st.title("CTF Simulation")
-    col1, _, col2 = st.beta_columns((3, 0.1, 2))
+    col1, col2 = st.beta_columns((3, 2))
     with col1:
         plot1d_s2 = st.checkbox(label='plot s^2 as x-axis', value=False)
 
-        s, s2, ctf = ctf1d(voltage, cs, ampcontrast, session_state.defocus, phaseshift, bfactor, alpha, cc, dE, dI, dZ, dXY, apix, imagesize, over_sample, plot_abs)
-
         from bokeh.plotting import figure
+        from bokeh.models import LegendItem
         if plot1d_s2:
-            x = s2
             x_label = "s^2 (1/Å^2)"
             hover_x_var = "s^2"
             hover_x_val = "$x 1/Å^2"
         else:
-            x = s
             x_label = "s (1/Å)"
             hover_x_var = "s"
             hover_x_val = "$x 1/Å"
         y_label = f"{ctf_type}"
-        source = dict(x=x, res=1/s, y=ctf)
+
         tools = 'box_zoom,crosshair,hover,pan,reset,save,wheel_zoom'
         hover_tips = [("Res", "@res Å"), (hover_x_var, hover_x_val), (f"{ctf_type}", "$y")]
-        fig = figure(title="", x_axis_label=x_label, y_axis_label=y_label, x_range=(0, x[-1]), tools=tools, tooltips=hover_tips)
-        fig.line(x='x', y='y', source=source, line_width=2)
+        fig = figure(title="", x_axis_label=x_label, y_axis_label=y_label, tools=tools, tooltips=hover_tips)
+
+        ll_line_dashes = 'dashed solid dotted dotdash dashdot'.split()        
+        if dfdiff:
+            defocuses = [session_state.defocus - dfdiff, session_state.defocus, session_state.defocus + dfdiff]
+        else:
+            defocuses = [session_state.defocus]
+        legends = []
+        raw_data = []
+        for di, defocus in enumerate(defocuses):
+            s, s2, ctf = ctf1d(voltage, cs, ampcontrast, defocus, phaseshift, bfactor, alpha, cc, dE, dI, dZ, dXY, apix, imagesize, over_sample, plot_abs, plot1d_s2)
+            x = s2 if plot1d_s2 else s
+            source = dict(x=x, res=1/s, y=ctf)
+            line_dash = ll_line_dashes[di] if len(defocuses)>1 else "solid"
+            line_width = 2 if len(defocuses)==1 or di==1 else 1
+            line = fig.line(x='x', y='y', source=source, line_dash=line_dash, line_width=line_width)
+            label = f"{round(defocus, 4)}µm"
+            legends.append(LegendItem(label=label, renderers=[line]))
+            raw_data.append((label, ctf))
+
+        if dfdiff and ctf_type=='CTF^2':
+            _, _, ctf_2d = ctf2d(voltage, cs, ampcontrast, session_state.defocus, dfdiff, dfang, phaseshift, bfactor, alpha, cc, dE, dI, dZ, dXY, apix, imagesize, over_sample, plot_abs, plot1d_s2)
+            rad_profile = compute_radial_profile(ctf_2d)
+            source = dict(x=s2 if plot1d_s2 else s, res=1/s, y=rad_profile)
+            line = fig.line(x='x', y='y', source=source, color='red', line_dash="solid", line_width=2)
+            label = "rotavg"
+            legends.append(LegendItem(label="rotavg", renderers=[line]))
+            raw_data.append((label, rad_profile))
+
+        fig.x_range.start = 0
+        fig.x_range.end = source['x'][-1]
+        fig.y_range.start = -1 if ctf_type == 'CTF' else 0
+        fig.y_range.end = 1
+        if len(legends)>1:
+            from bokeh.models import Legend
+            legend = Legend(items=legends)
+            fig.add_layout(legend)
+            fig.legend[0].location = "top_left"
+            fig.legend.click_policy= "hide"
+            from bokeh.models import CustomJS
+            from bokeh.events import MouseMove, DoubleTap
+            toggle_legend_js = CustomJS(args=dict(leg=fig.legend[0]), code="""
+                if (leg.visible) {
+                    leg.visible = false
+                    }
+                else {
+                    leg.visible = true
+                }
+            """)
+            fig.js_on_event(DoubleTap, toggle_legend_js)
         st.bokeh_chart(fig, use_container_width=True)
 
         show_data = st.checkbox('show raw data', value=False)
         if show_data:
             import pandas as pd
-            data = np.zeros((len(x), 3))
+            data = np.zeros((len(x), 2 + len(raw_data)))
             data[:,0] = x
             data[:,1] = 1./s
-            data[:,2] = ctf
-            df = pd.DataFrame(data, columns=(x_label.rjust(15), "Res (Å)".rjust(15), y_label.rjust(15)))
-            st.dataframe(df, width=600)
+            columns = [x_label, "Res (Å)"]
+            for i, (label, ctf) in enumerate(raw_data):
+                data[:,2+i] = ctf
+                if len(raw_data)>1:
+                    columns.append(y_label+' '+label)
+                else:
+                    columns.append(y_label)
+            columns = [col.rjust(12) for col in columns]
+            df = pd.DataFrame(data, columns=columns)
+            st.dataframe(df, width=None)
             st.markdown(get_table_download_link(df), unsafe_allow_html=True)
 
         st.markdown("*Developed by the [Jiang Lab@Purdue University](https://jiang.bio.purdue.edu). Report problems to Wen Jiang (jiang12 at purdue.edu)*")
@@ -80,11 +173,10 @@ def main():
         plot2d_s2 = st.checkbox(label='plot s^2 as radius', value=False)
         st.text("") # workaround for a layout bug in streamlit 
 
-        (ctf, ds), (ctf_s2, ds2) = ctf2d(voltage, cs, ampcontrast, session_state.defocus, dfdiff, dfang, phaseshift, bfactor, alpha, cc, dE, dI, dZ, dXY, apix, imagesize, over_sample, plot_abs, plot2d_s2)
-        ctf_to_plot = ctf_s2 if plot2d_s2 else ctf
+        ds, ds2, ctf_2d = ctf2d(voltage, cs, ampcontrast, session_state.defocus, dfdiff, dfang, phaseshift, bfactor, alpha, cc, dE, dI, dZ, dXY, apix, imagesize, over_sample, plot_abs, plot2d_s2)
         dxy = ds2 if plot2d_s2 else ds
-
-        w, h = ctf_to_plot.shape
+    
+        w, h = ctf_2d.shape
         tools = 'box_zoom,crosshair,pan,reset,save,wheel_zoom'
         fig2d = figure(frame_width=w, frame_height=h,
             x_range=(-w//2*dxy, (w//2-1)*dxy), y_range=(-h//2*dxy, (h//2-1)*dxy),
@@ -93,32 +185,21 @@ def main():
         fig2d.axis.visible = False
         fig2d.toolbar_location = None
 
-        sx_grid, sy_grid = np.meshgrid(np.arange(-w//2, w//2)*ds, np.arange(-h//2, h//2)*ds, indexing='ij', copy=False)
-        ang_grid = np.fmod(-np.rad2deg(np.arctan2(sy_grid, sx_grid))+90.+360., 360.)
-        s_grid = np.hypot(sx_grid, sy_grid)
-        res_grid = 1./s_grid
-        
         if plot2d_s2:
-            s2x_grid, s2y_grid = np.meshgrid(np.arange(-w//2, w//2)*ds2, np.arange(-h//2, h//2)*ds2, indexing='ij', copy=False)
-            s2_grid = np.hypot(s2x_grid, s2y_grid)
-            s_grid = np.sqrt(s2_grid)
-
-            source_data = dict(ctf=[ctf_to_plot], x=[-w//2*dxy], y=[-h//2*dxy], dw=[w*dxy], dh=[h*dxy], 
-                res=[res_grid], s=[s_grid], s2=[s2_grid], ang=[ang_grid])
+            source_data = dict(ctf=[ctf_2d], x=[-w//2*dxy], y=[-h//2*dxy], dw=[w*dxy], dh=[h*dxy])
             tooltips = [
                 ("Res", "@res Å"),
                 ("s", "@s 1/Å"),
                 ("s2", "@s2 1/Å^2"),
-                ('ang', '@ang °'),
+                ('angle', '@ang °'),
                 (ctf_type, '@ctf')
             ]
         else:
-            source_data = dict(ctf=[ctf_to_plot], x=[-w//2*dxy], y=[-h//2*dxy], dw=[w*dxy], dh=[h*dxy],
-                res=[res_grid], s=[s_grid], ang=[ang_grid])
+            source_data = dict(ctf=[ctf_2d], x=[-w//2*dxy], y=[-h//2*dxy], dw=[w*dxy], dh=[h*dxy])
             tooltips = [
                 ("Res", "@res Å"),
                 ("s", "@s 1/Å"),
-                ('ang', '@ang °'),
+                ('angle', '@ang °'),
                 (ctf_type, '@ctf')
             ]
 
@@ -130,6 +211,35 @@ def main():
         from bokeh.models.tools import HoverTool
         image_hover = HoverTool(renderers=[fig2d_image], tooltips=tooltips)
         fig2d.add_tools(image_hover)
+
+        # avoid the need for embedding res/s/s2 image -> smaller fig object and less data to transfer
+        from bokeh.models import CustomJS
+        from bokeh.events import MouseMove
+        mousemove_callback_code = """
+        var x = cb_obj.x
+        var y = cb_obj.y
+        var angle = Math.round(Math.atan2(y, x)*180./Math.PI * 100)/100
+        console.log(x, y, angle)
+        var s, res, s2
+        if(s2) {
+            s2 = Math.round(Math.hypot(x, y) * 1e3)/1e3
+            s = Math.round(Math.sqrt(s2) * 1e3)/1e3
+            res = Math.round(1./s * 100)/100
+            hover.tooltips[0][1] = res.toString() + " Å"
+            hover.tooltips[1][1] = s.toString() + " Å"
+            hover.tooltips[2][1] = s2.toString() + " 1/Å^2"
+            hover.tooltips[3][1] = angle.toString() + " °"
+        } 
+        else {
+            s = Math.round(Math.hypot(x, y) * 1e3)/1e3
+            res = Math.round(1./s * 100)/100
+            hover.tooltips[0][1] = res.toString() + " Å"
+            hover.tooltips[1][1] = s.toString() + " 1/Å"
+            hover.tooltips[2][1] = angle.toString() + " °"
+        }
+        """
+        mousemove_callback = CustomJS(args={"hover":fig2d.hover[0], "s2":plot2d_s2}, code=mousemove_callback_code)
+        fig2d.js_on_event(MouseMove, mousemove_callback)
 
         st.bokeh_chart(fig2d, use_container_width=True)
         
@@ -171,17 +281,23 @@ def main():
             st.warning(f"{input_txt} is not a valid image link")
         if image is not None:
             st.markdown(link, unsafe_allow_html=True)
-            st.image(image, caption="Orignal image", clamp=[image.min(), image.max()])
+            st.image(normalize(image), caption="Orignal image", clamp=True)
             # apply ctf to the image
-            image2 = np.abs(np.fft.ifft2(np.fft.fft2(image)*np.fft.fftshift(ctf)))
-            clamp_min, clamp_max = np.percentile(image2, (5, 95))
-            st.image(image2, caption=f"{ctf_type} applied", clamp=(clamp_min, clamp_max))
+            _, _, ctf_2d = ctf2d(voltage, cs, ampcontrast, session_state.defocus, dfdiff, dfang, phaseshift, bfactor, alpha, cc, dE, dI, dZ, dXY, apix, imagesize, over_sample, abs=plot_abs, plot_s2=False)
+            image2 = np.abs(np.fft.ifft2(np.fft.fft2(image)*np.fft.fftshift(ctf_2d)))
+            st.image(normalize(image2), caption=f"{ctf_type} applied", clamp=True)
 
 @st.cache(persist=True, show_spinner=False)
-def ctf1d(voltage, cs, ampcontrast, defocus, phaseshift, bfactor, alpha, cc, dE, dI, dZ, dXY, apix, imagesize, over_sample, abs):
-    ds = 1./(apix*imagesize*over_sample)
-    s = np.arange(imagesize*over_sample//2+1, dtype=np.float)*ds
-    s2 = s*s
+def ctf1d(voltage, cs, ampcontrast, defocus, phaseshift, bfactor, alpha, cc, dE, dI, dZ, dXY, apix, imagesize, over_sample, abs, plot_s2=False):
+    s_nyquist = 1./(2*apix)
+    if plot_s2:
+        ds2 = s_nyquist*s_nyquist/(imagesize//2*over_sample)
+        s2 = np.arange(imagesize//2*over_sample+1, dtype=np.float)*ds2
+        s = np.sqrt(s2)
+    else:
+        ds = s_nyquist/(imagesize//2*over_sample)
+        s = np.arange(imagesize//2*over_sample+1, dtype=np.float)*ds
+        s2 = s*s
     wl = 12.2639 / np.sqrt(voltage * 1000.0 + 0.97845 * voltage * voltage)  # Angstrom
     wl3 = np.power(wl, 3)
     phaseshift = phaseshift * np.pi / 180.0 + np.arcsin(ampcontrast/100.)
@@ -203,24 +319,35 @@ def ctf1d(voltage, cs, ampcontrast, defocus, phaseshift, bfactor, alpha, cc, dE,
     return s, s2, ctf
 
 @st.cache(persist=True, show_spinner=False)
-def ctf2d(voltage, cs, ampcontrast, defocus, dfdiff, dfang, phaseshift, bfactor, alpha, cc, dE, dI, dZ, dXY, apix, imagesize, over_sample, abs, plot_s2=False):
-    ds = 1./(apix*imagesize*over_sample)
-    sx = np.arange(-imagesize*over_sample//2, imagesize*over_sample//2) * ds
-    sy = np.arange(-imagesize*over_sample//2, imagesize*over_sample//2) * ds
-    sx, sy = np.meshgrid(sx, sy, indexing='ij')
+def ctf2d(voltage, cs, ampcontrast, defocus, dfdiff, dfang, phaseshift, bfactor, alpha, cc, dE, dI, dZ, dXY, apix, imagesize, over_sample, abs, plot_s2=False):    
+    s_nyquist = 1./(2*apix)
+    if plot_s2:
+        ds = None
+        ds2 = s_nyquist*s_nyquist/(imagesize//2*over_sample)
+        sx2 = np.arange(-imagesize*over_sample//2, imagesize*over_sample//2) * ds2
+        sy2 = np.arange(-imagesize*over_sample//2, imagesize*over_sample//2) * ds2
+        sx2, sy2 = np.meshgrid(sx2, sy2, indexing='ij')
+        theta = -np.arctan2(sy2, sx2)
+        s2 = np.hypot(sx2, sy2)
+        s = np.sqrt(s2)
+    else:
+        ds2 = None
+        ds = s_nyquist/(imagesize//2*over_sample)
+        sx = np.arange(-imagesize*over_sample//2, imagesize*over_sample//2) * ds
+        sy = np.arange(-imagesize*over_sample//2, imagesize*over_sample//2) * ds
+        sx, sy = np.meshgrid(sx, sy, indexing='ij')
+        theta = -np.arctan2(sy, sx)
+        s2 = sx*sx + sy*sy
+        s = np.sqrt(s2)
 
-    theta = -np.arctan2(sy, sx)
     defocus2d = defocus + dfdiff/2*np.cos( 2*(theta-dfang*np.pi/180.))
 
     wl = 12.2639 / np.sqrt(voltage * 1000.0 + 0.97845 * voltage * voltage)  # Angstrom
-    wl3 = np.power(wl, 3)
     phaseshift = phaseshift * np.pi / 180.0 + np.arcsin(ampcontrast/100.)
 
-    s2 = sx*sx + sy*sy
     gamma =2*np.pi*(-0.5*defocus2d*1e4*wl*s2 + .25*cs*1e7*wl**3*s2**2) - phaseshift
 
     from scipy.special import j0, sinc
-    s = np.sqrt(s2)
     env = np.ones_like(gamma)
     if bfactor: env *= np.exp(-bfactor*s2/4.0)
     if alpha: env *= np.exp(-np.power(np.pi*alpha*(1.0e7*cs*wl*wl*s*s*s-1e4*defocus*s), 2.0)*1e-6)
@@ -233,20 +360,35 @@ def ctf2d(voltage, cs, ampcontrast, defocus, dfdiff, dfang, phaseshift, bfactor,
     if abs>=2: ctf = ctf*ctf
     elif abs==1: ctf = np.abs(ctf)
 
-    if plot_s2:
-        ds2 = np.power(1./(2*apix), 2)/(imagesize*over_sample//2)
-        s2x = np.arange(-imagesize*over_sample//2, imagesize*over_sample//2) * ds2
-        s2y = np.arange(-imagesize*over_sample//2, imagesize*over_sample//2) * ds2
-        s2x, s2y = np.meshgrid(s2x, s2y, indexing='ij')
-        s2 = np.hypot(s2x, s2y)
-        gamma =2*np.pi*(-0.5*defocus2d*1e4*wl*s2 + .25*cs*1e7*wl**3*s2**2) - phaseshift
-        ctf_s2 = np.sin(gamma) * np.exp(-bfactor*s2/4.0) 
-        if abs>=2: ctf_s2 = ctf_s2*ctf_s2
-        elif abs==1: ctf_s2 = np.abs(ctf_s2)
-    else:
-        ctf_s2 = None
-        ds2 = None
-    return (ctf, ds), (ctf_s2, ds2)
+    return ds, ds2, ctf
+
+@st.cache(persist=True, show_spinner=False)
+def compute_radial_profile(image):
+    ny, nx = image.shape
+    rmax = min(nx//2, ny//2)+1
+    
+    r = np.arange(0, rmax, 1, dtype=np.float32)
+    theta = np.arange(0, 360, 1, dtype=np.float32) * np.pi/180.
+    n_theta = len(theta)
+
+    theta_grid, r_grid = np.meshgrid(theta, r, indexing='ij', copy=False)
+    y_grid = ny//2 + r_grid * np.sin(theta_grid)
+    x_grid = nx//2 + r_grid * np.cos(theta_grid)
+
+    coords = np.vstack((y_grid.flatten(), x_grid.flatten()))
+
+    from scipy.ndimage.interpolation import map_coordinates
+    polar = map_coordinates(image, coords, order=1).reshape(r_grid.shape)
+
+    rad_profile = polar.mean(axis=0)
+    return rad_profile
+
+@st.cache(persist=True, show_spinner=False)
+def normalize(data, percentile=(0, 100)):
+    p0, p1 = percentile
+    vmin, vmax = sorted(np.percentile(data, (p0, p1)))
+    data2 = (data-vmin)/(vmax-vmin)
+    return data2
 
 @st.cache(persist=True, show_spinner=False, ttl=24*60*60.) # refresh every day
 def get_emdb_ids():
@@ -300,16 +442,13 @@ def get_table_download_link(df):
 @st.cache(persist=True, show_spinner=False)
 def setup_anonymous_usage_tracking():
     try:
-        import os, stat
-        index_file = os.path.dirname(st.__file__) + "/static/index.html"
-        os.chmod(index_file, stat.S_IRUSR|stat.S_IWUSR|stat.S_IROTH)
-        with open(index_file, "r+") as fp:
-            txt = fp.read()
-            if txt.find("gtag.js")==-1:
-                txt2 = txt.replace("<head>", '''<head><!-- Global site tag (gtag.js) - Google Analytics --><script async src="https://www.googletagmanager.com/gtag/js?id=G-YV3ZFR8VG6"></script><script>window.dataLayer = window.dataLayer || [];function gtag(){dataLayer.push(arguments);}gtag('js', new Date());gtag('config', 'G-YV3ZFR8VG6');</script>''')
-                fp.seek(0)
-                fp.write(txt2)
-                fp.truncate()
+        import pathlib, stat
+        index_file = pathlib.Path(st.__file__).parent / "static/index.html"
+        index_file.chmod(stat.S_IRUSR|stat.S_IWUSR|stat.S_IRGRP|stat.S_IROTH)
+        txt = index_file.read_text()
+        if txt.find("gtag/js?")==-1:
+            txt = txt.replace("<head>", '''<head><script async src="https://www.googletagmanager.com/gtag/js?id=G-8Z99BDVHTC"></script><script>window.dataLayer = window.dataLayer || [];function gtag(){dataLayer.push(arguments);}gtag('js', new Date());gtag('config', 'G-8Z99BDVHTC');</script>''')
+            index_file.write_text(txt)
     except:
         pass
 
