36efb4de22b1f5a1abc3e40de5cbd1e76f8652c9
Wen Jiang <wjiang@gmail.com>
2022-02-13 17:51:33 -0500

    bump streamlit to v1.5.1

--- ctf_simulation.py
+++ ctf_simulation.py
@@ -1,7 +1,7 @@
 """ 
 MIT License
 
-Copyright (c) 2020-2021 Wen Jiang
+Copyright (c) 2020-2022 Wen Jiang
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
